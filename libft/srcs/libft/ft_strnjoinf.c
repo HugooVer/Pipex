b9a0baeb96fff4c75c91f80ddb5a4326f2ca3ad3
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-13 12:15:00 +0200

    typo mistake

--- libft/srcs/libft/ft_strnjoinf.c
+++ libft/srcs/libft/ft_strnjoinf.c
@@ -6,13 +6,13 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 18:10:04 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/12 18:11:21 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/13 12:12:48 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnjoinf1(char *s1, char *s2, size_t n)
+char	*ft_strnjoinf(char *s1, char *s2, size_t n)
 {
 	size_t	len1;
 	size_t	len2;
